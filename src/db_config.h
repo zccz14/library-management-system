f7c66754a6abe0612513911174d69303d4e4145e
Izumo Riki <zccz14@outlook.com>
2016-04-19 15:01:17 +0800

    ADD Database Connection Config

---
+++ src/db_config.h
@@ -0,0 +1,10 @@
+// Database Connection Config
+// Database Host
+#define DB_HOST "localhost"
+// Database Username
+#define DB_USER "root"
+// Database Password
+#define DB_PASS "password"
+// Database Name
+#define DB_NAME "library_management"
+
